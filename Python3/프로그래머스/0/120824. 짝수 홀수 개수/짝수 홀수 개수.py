e34e8acbd0796e4a34822e2372b67b7aeef2b82b
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-10-04 17:03:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ Python3/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,12 @@
 def solution(num_list):
-    answer = []
+    k = 0
+    t = 0
+    answer = [k,t] 
+    for i in num_list:
+        if i%2 == 0:
+            k +=1
+        else:
+            t +=1
+        answer[0] = k 
+        answer[1] = t
     return answer
