1509902e91d30437c27fced79f55c4439f8abdd5
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-11-03 06:59:34 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ Java/프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int solution(int[] num_list, int n) {
-        for(int i = 0; i < num_list.length; i++) {
-            if(num_list[i] == n) {
-                return 1; 
+    public int solution(int[] num_list, Integer n){
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]==n){
+                return 1;
             }
         }
-        return 0; 
+        return 0;
     }
 }
