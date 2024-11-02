9117b5303619cb8df055fb0bbe6a5ffcf2f4cbd6
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-11-02 16:26:57 +0900

    [level 0] Title: 부분 문자열, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ Java/프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int solution(String str1, String str2) { 
-        int answer = 0;
+    public int solution(String str1, String str2){
         if(str2.contains(str1)){
             return 1;
         }
