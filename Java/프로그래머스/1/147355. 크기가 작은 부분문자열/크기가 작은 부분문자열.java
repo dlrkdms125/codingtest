95322ecd3255b3a22afc133f73142b7102a94f9e
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-12-05 20:19:40 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 10.93 ms, Memory: 89.5 MB -BaekjoonHub

--- Java/프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ Java/프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,16 +1,6 @@
 class Solution {
     public int solution(String t, String p) {
-        int length = p.length();
-        long number = Long.parseLong(p);
-        int result = 0;
-        
-        for(int i=0; i<t.length()-length+1; i++){
-            long newNumber = Long.parseLong(t.substring(i, i+length));
-            if(newNumber<=number){
-                result++;
-            }
-        
-        }
-        return result;
+        int answer = 0;
+        return answer;
     }
 }
