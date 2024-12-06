0bf36be14ea30e21b2a5a5fc4d9754cc2e31bee4
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-12-06 18:12:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 301.30 ms, Memory: 96.2 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,15 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
         int i=0;
         Arrays.sort(participant);
         Arrays.sort(completion);
-
-        for(i=0; i<completion.length; i++)
-            if(!completion[i].equals(participant[i])){
-                break;
-            }
+        
+        for(i=0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])) break;
+            
+        }
+    
         return participant[i];
     }
 }
