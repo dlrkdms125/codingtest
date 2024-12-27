e8691f20c9d37d9efeeecb93882bdd48b5989f04
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-12-27 19:15:34 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

---
+++ Java/백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine()); 
+
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            if (isValid(s)) bw.write("YES\n");
+            else bw.write("NO\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false; 
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.empty();
+    }
+}
