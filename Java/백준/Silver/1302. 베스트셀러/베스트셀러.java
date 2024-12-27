d4685a0a849c01424f572b44094efcef343bf045
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2024-12-27 23:16:27 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ Java/백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            String book = br.readLine();
+            map.put(book, map.getOrDefault(book, 0) + 1);
+            max = Math.max(max, map.get(book));
+        }
+
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                list.add(entry.getKey());
+            }
+        }
+
+        Collections.sort(list);
+        bw.write(list.get(0));
+        bw.newLine();
+
+        br.close();
+        bw.close();
+    }
+}
