f0f2fd025ec955971ac48b951651260cc7404e93
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-01-03 18:39:07 +0900

    [Silver V] Title: 올림픽, Time: 140 ms, Memory: 16260 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/8979. 올림픽/올림픽.java
+++ Java/백준/Silver/8979. 올림픽/올림픽.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int nation = Integer.parseInt(st.nextToken());
         int target = Integer.parseInt(st.nextToken());
@@ -18,32 +19,35 @@ public class Main {
             }
         }
 
+        // 금, 은, 동 내림차순 정렬
         Arrays.sort(arrs, (o1, o2) -> {
             if (o1[1] == o2[1]) {
                 if (o1[2] == o2[2]) {
                     return Integer.compare(o2[3], o1[3]); 
                 }
-                return Integer.compare(o2[2], o1[2]); 
+                return Integer.compare(o2[2], o1[2]);
             }
             return Integer.compare(o2[1], o1[1]); 
         });
 
-        int endpoint = 0;
-        int rank = 0;
-
+        int endpoint = -1;
         for (int i = 0; i < nation; i++) {
             if (arrs[i][0] == target) {
                 endpoint = i;
                 break;
             }
         }
-        for (int i = 0; i < nation; i++) {
-            if (arrs[i][1] == arrs[endpoint][1] && arrs[i][2] == arrs[endpoint][2] && arrs[i][3] == arrs[endpoint][3]) {
+        if (endpoint == -1) { 
+            throw new IllegalArgumentException("Target nation not found");
+        }
+
+        int rank = 1;
+        for (int i = 0; i < endpoint; i++) {
+            if (!(arrs[i][1] == arrs[endpoint][1] && arrs[i][2] == arrs[endpoint][2] && arrs[i][3] == arrs[endpoint][3])) {
                 rank++;
-            } else if (i > endpoint) {
-                break;
             }
         }
+
         bw.write(rank + "\n");
         bw.flush();
     }
