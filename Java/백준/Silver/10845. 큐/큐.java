f1d086aca8a7d991d5c534e2e46735fabd3ca3e7
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-01-11 06:26:26 +0900

    [Silver IV] Title: 큐, Time: 188 ms, Memory: 19992 KB -BaekjoonHub

---
+++ Java/백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        int last = 0;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    last = Integer.parseInt(st.nextToken());
+                    queue.offer(last);
+                    break;
+
+                case "pop":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.poll() + "\n");
+                    }
+                    break;
+
+                case "size":
+                    bw.write(queue.size() + "\n");
+                    break;
+
+                case "empty":
+                    bw.write(queue.isEmpty() ? "1\n" : "0\n");
+                    break;
+
+                case "front":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.peek() + "\n");
+                    }
+                    break;
+
+                case "back":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(last + "\n");
+                    }
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
