57c2277feeedee307e1141d8bcbc2978690e0f9f
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-02-24 21:27:50 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) { 
+                count++;
+                if (count == k) {
+                    bw.write(String.valueOf(i)); 
+                    bw.flush();
+                    bw.close();
+                    br.close();
+                    return; 
+                }
+            }
+        }
+        bw.write("0");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
