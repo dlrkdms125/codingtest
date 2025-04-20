87d18de34a382b280075bf5e98e67f4c035f14d8
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-20 17:22:15 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.55 ms, Memory: 82 MB -BaekjoonHub

--- Java/프로그래머스/0/181852. 뒤에서 5등 위로/README.md
+++ Java/프로그래머스/0/181852. 뒤에서 5등 위로/README.md
@@ -1,10 +1,10 @@
 # [level 0] 뒤에서 5등 위로 - 181852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181852?language=java) 
 
 ### 성능 요약
 
-메모리: 69.2 MB, 시간: 1.17 ms
+메모리: 82 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 03일 08:14:39
+2025년 04월 20일 17:22:12
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,13 +1,12 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
+        int n = num_list.length;
+        int[] answer = new int[n-5];
         Arrays.sort(num_list);
-        
-        int[] num = new int[num_list.length-5];
-        for(int i=5; i<num_list.length; i++){
-            num[i-5] = num_list[i];
+        for(int i=5; i<n; i++){
+            answer[i-5] = num_list[i];
         }
-        return num;
+        return answer;
     }
 }
