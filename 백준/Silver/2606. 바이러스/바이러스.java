cf181968232ce9b8adf6b1ad0338b14bc5a85c6d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-25 09:00:20 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] check;
+    static int[][] arr;
+    static int count = 0;
+    static int node, line;
+    static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        node = Integer.parseInt(br.readLine());
+        line = Integer.parseInt(br.readLine());
+        arr = new int[node+1][node+1];
+        check = new boolean[node+1];
+        for (int i=0; i<line; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = arr[b][a] = 1;
+        }
+        dfs(1);
+        bw.write(String.valueOf(count-1));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    public static void dfs(int start){
+        check[start] = true;
+        count++;
+        for(int i=0; i<=node; i++){
+            if(arr[start][i] == 1 && !check[i]) dfs(i);
+        }
+    }
+}
