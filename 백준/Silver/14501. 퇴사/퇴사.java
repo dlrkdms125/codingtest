43856f4ad142c675480745755514c39818a2ff58
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-29 13:55:46 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,44 +3,39 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static int[][] schedule;
-    static int result;
-    static int n;
+  
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        schedule = new int[n][2];
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n+1];
+        int[] p = new int[n+1];
+        int[] dp = new int[n+2];
         
-        int maxHeight = 0;
-        for(int i=0; i<n; i++){
+        for(int i=1; i<=n; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            schedule[i][0] = Integer.parseInt(st.nextToken());
-            schedule[i][1] = Integer.parseInt(st.nextToken());
+           t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
-        result = 0;
-        dfs(0,0);
-        
-        bw.write(String.valueOf(result));
+        int result = 0;
+    
+        for(int i=n; i>0; i--){
+            int next = i+t[i]; // 다음 날짜
+            if(next>n+1){ // 일할 수 있는 날짜 넘어가면
+                dp[i] = dp[i+1]; // 일을 하지 못하므로 바로 다음 dp값로 설정
+            } else {
+                // 일하지 않았을 때 dp[i+1]
+                // 일했을때 총 벌수 있는 금액 p[i]+dp[next]
+                dp[i] = Math.max(dp[i+1], p[i]+dp[next]);
+            }
+            
+        }
+        bw.write(String.valueOf(dp[1]));
         bw.flush();
         bw.close();
         br.close();
-        
-     
+    
     }
-    public static void dfs(int idx, int pay){ // 날짜와 이익을 입력받아서
-       if(idx>=n){ // 날짜가 n이상이면 result 중 최대값을 구하며 끝낸다.
-           result = Math.max(pay,result);
-           return;
-       }
-        // 상담을 끝난 날이 n 이하면, dfs에 상담이 끝난 날짜와 합친 상담비를 넣는다.
-        if(idx+schedule[idx][0]<=n){ 
-            dfs(idx+schedule[idx][0], pay+schedule[idx][1]);
-        } else { // 상담을 끝낸 날이 n을 넘어가면, dfs에 상담이 끝난 날짜만 넘겨준다. 합친 상담비는 그대로고, 넘겨준 날짜는 탈출조건에서 사용한다/
-            dfs(idx+schedule[idx][0], pay);
-        }
-        // dfs(idx+1, pay)를 dfs 끝에 넣어주어 이어서 상담하지 않고 날짜를 띄어서 새로운 날짜를 탐색할 수 있도록 해준다. 마지막 날짜까지 모두 탐색할 수 있음.
-        dfs(idx+1, pay);
-    }
+    
 }
