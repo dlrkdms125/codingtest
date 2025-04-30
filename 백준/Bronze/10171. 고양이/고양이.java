23463758a888eb32034c3b511fafa94d70cce78d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-30 10:44:58 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // BigInteger A = new BigInteger(st.nextToken());
+        // BigInteger B = new BigInteger(st.nextToken());
+        // BigInteger C = new BigInteger(st.nextToken());
+
+    
+        bw.write("\\    /\\");
+        bw.newLine();
+        bw.write(" )  ( ')");
+        bw.newLine();
+        bw.write("(  /  )");
+        bw.newLine();
+        bw.write(" \\(__)|");
+        bw.newLine();
+    
+        bw.flush();
+        bw.close();
+        // br.close();
+    }
+}
