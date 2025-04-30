492d87d65852daffa784a34e9d391a7dbb93202b
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-30 10:47:47 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // BigInteger A = new BigInteger(st.nextToken());
+        // BigInteger B = new BigInteger(st.nextToken());
+        // BigInteger C = new BigInteger(st.nextToken());
+
+    
+        bw.write("|\\_/|");
+        bw.newLine();
+        bw.write("|q p|   /}");
+        bw.newLine();
+        bw.write("( 0 )\"\"\"\\");
+        bw.newLine();
+        bw.write("|\"^\"`    |");
+        bw.newLine();
+        bw.write("||_/=\\\\__|");
+        bw.newLine();
+    
+        bw.flush();
+        bw.close();
+        // br.close();
+    }
+}
