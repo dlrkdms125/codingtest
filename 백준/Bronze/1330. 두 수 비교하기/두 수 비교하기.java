97552a289b4e09dafb460b5db2a324bfe819310d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-30 10:50:47 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if(a>b){ bw.write(">"); }
+        else if (a<b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+    
+    
+        bw.flush();
+        bw.close();
+        // br.close();
+    }
+}
