c6416299ea57677d993478d03de6e0909c4fc940
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-04-30 13:08:06 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a+b;
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+    
+        
+        bw.flush();
+        bw.close();
+        // br.close();
+    }
+}
