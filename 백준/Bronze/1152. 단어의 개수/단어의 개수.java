8813f01e74f1830e651da656357acd9b830ad6d7
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-02 12:41:20 +0900

    [Bronze II] Title: 단어의 개수, Time: 228 ms, Memory: 20152 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        
+
+        bw.write(String.valueOf(st.countTokens()));
+    
+        bw.flush();
+        bw.close();
+        br.close();
+    }    
+}
