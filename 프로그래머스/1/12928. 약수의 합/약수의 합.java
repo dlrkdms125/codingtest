21ee02d03b77673bd32518a3915f9fd9968232f1
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-08 09:45:47 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 class Solution {
-    public int solution(int n) {
-        int sum = 0;
-        for(int i=1; i<=n; i++){
-            if(n%i==0) sum += i;
+    public int solution(int n){
+        int answer = 0;
+        for(int i=1; i<=n/2; i++){
+            if(n%i==0) answer += i;
         }
-        return sum;
+        return answer+n;
     }
 }
