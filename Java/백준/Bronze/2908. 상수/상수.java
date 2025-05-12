818ae8e9c7139a2621e66a66bb608e4d748c6d29
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-12 22:23:12 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14384 KB -BaekjoonHub

--- Java/백준/Bronze/2908. 상수/상수.java
+++ Java/백준/Bronze/2908. 상수/상수.java
@@ -1,19 +1,18 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] str = br.readLine().split(" ");
+
+        // 문자열 뒤집고 → 숫자로 변환
         int num1 = Integer.parseInt(new StringBuilder(str[0]).reverse().toString());
         int num2 = Integer.parseInt(new StringBuilder(str[1]).reverse().toString());
+
+        // 큰 수 출력
         bw.write(String.valueOf(Math.max(num1, num2)));
-        bw.flush();
         bw.close();
         br.close();
-        
     }
 }
