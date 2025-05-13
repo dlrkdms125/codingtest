0b5089067f39d7cdc0792746c156dc0b506aca7a
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-13 09:30:16 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- Java/백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ Java/백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -4,19 +4,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[46];
         arr[0] = 0;
         arr[1] = 1;
-
-        for (int i = 2; i < 46; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
+        for (int i = 2; i <= n; i++){
+            arr[i] = arr[i-2]+arr[i-1];
         }
-
-        int T = Integer.parseInt(br.readLine());
-        bw.write(String.valueOf(arr[T]));
+        bw.write(String.valueOf(arr[n]));
         bw.flush();
         bw.close();
         br.close();
+
     }
 }
