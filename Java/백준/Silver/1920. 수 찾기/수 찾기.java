feb7778ce4816a2ee2603b9d976ac8bb359a98ef
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-14 13:27:46 +0900

    [Silver IV] Title: 수 찾기, Time: 588 ms, Memory: 42644 KB -BaekjoonHub

---
+++ Java/백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,42 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        Arrays.sort(arr);
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if(binarySearch(arr, target)) bw.write("1\n");
+            else bw.write("0\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+    private static boolean binarySearch(int[] arr, int target){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(arr[mid]==target) return true;
+            else if(arr[mid]<target) left = mid+1;
+            else right = mid-1;
+        }
+        return false;
+    }
+}
