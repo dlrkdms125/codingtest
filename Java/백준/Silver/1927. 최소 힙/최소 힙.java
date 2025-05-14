fb916a792f6b1922acd4b797892ea3326ef9e96d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-14 16:13:21 +0900

    [Silver II] Title: 최소 힙, Time: 296 ms, Memory: 31764 KB -BaekjoonHub

--- Java/백준/Silver/1927. 최소 힙/최소 힙.java
+++ Java/백준/Silver/1927. 최소 힙/최소 힙.java
@@ -3,19 +3,22 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> q = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if(num==0) bw.write((q.size()==0 ? 0: q.poll())+"\n");
-            else q.add(num);
+            int number = Integer.parseInt(br.readLine());
+            if(number>0) pq.add(number);
+            else if(number==0) {
+                if(!pq.isEmpty()) bw.write(String.valueOf(pq.poll())+"\n");
+                else bw.write(String.valueOf(0)+"\n");
+            }
         }
         bw.flush();
         bw.close();
         br.close();
-
     }
 }
