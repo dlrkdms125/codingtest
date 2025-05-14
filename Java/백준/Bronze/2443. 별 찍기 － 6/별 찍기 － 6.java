368bb6c6c80d8012b6c5ee64ee66a613f3442653
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-14 10:24:57 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 112 ms, Memory: 14308 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+
+            for (int j = 0; j < n - i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 2 * i - 1; j > 0; j--) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
