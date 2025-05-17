848219c4d77559c7d5b7284e7721ce509a8051c5
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-17 14:15:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 120 ms, Memory: 15900 KB -BaekjoonHub

--- Java/백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ Java/백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -6,30 +6,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] bucket = new int[n];
-
+        int[] basket = new int[n];
         for (int i = 0; i < n; i++) {
-            bucket[i] = i+1;
+            basket[i] = i+1;
         }
-
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int I = Integer.parseInt(st.nextToken());
-            int J = Integer.parseInt(st.nextToken());
-
-            int temp = bucket[I-1];
-            bucket[I-1] = bucket[J-1];
-            bucket[J-1] = temp;
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            int temp = basket[a];
+            basket[a] = basket[b];
+            basket[b] = temp;
         }
         for (int i = 0; i < n; i++) {
-            bw.write(bucket[i]+" ");
+            bw.write(basket[i]+" ");
         }
         bw.flush();
         bw.close();
         br.close();
+
 
     }
 }
