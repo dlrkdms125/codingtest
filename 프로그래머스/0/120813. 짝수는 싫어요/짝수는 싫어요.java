71dc2e1491d8cb4aa23633a56ee4bf32d1a7c399
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-24 22:37:24 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=n; i=i+2){
+            list.add(i);
+        }
+        int[] arr = new int[list.size()];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
