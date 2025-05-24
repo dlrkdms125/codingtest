f0fa4cc74f02e289c47db50bbc48c60f2f1449c1
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-24 22:16:39 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<rsp.length(); i++){
+            char ch = rsp.charAt(i);
+            if(ch=='2') answer.append('0');
+            else if(ch=='0') answer.append('5');
+            else answer.append('2');
+        }
+        return answer.toString();
+    }
+}
