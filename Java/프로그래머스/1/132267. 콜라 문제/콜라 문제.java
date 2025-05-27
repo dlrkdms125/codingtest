f7286a6f4cb8b997dc790b258ae0debe8b9afefe
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-27 11:07:31 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 87.1 MB -BaekjoonHub

--- Java/프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ Java/프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        int result = 0;
+        int answer = 0;
         while(n>=a){
-            result += ((n/a)*b);
-            n = ((n/a)*b) + n%a;
+            answer += n/a*b;
+            n = n/a*b+n%a;
         }
-        
-        return result;
+        return answer;
     }
 }
