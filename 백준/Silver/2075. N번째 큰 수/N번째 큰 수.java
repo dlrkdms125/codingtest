af28daac5fa00efc7cfede71ec63dc88c3ca0108
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-05-30 15:42:10 +0900

    [Silver III] Title: N번째 큰 수, Time: 776 ms, Memory: 274100 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for(int i=0;i<n-1;i++) {
+            pq.remove();
+        }
+        bw.write(pq.poll()+"");
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
