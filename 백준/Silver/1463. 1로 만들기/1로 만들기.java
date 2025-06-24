d9d7ec45a924c14c24429b0d5eac1a53825bc3d0
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-06-24 10:01:50 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,20 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 0;
-
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1]+1;
-            if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
-            if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+        int[] dp = new int[n + 1];
+  
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1; // 1을 빼는 경우
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 2로 나누는 경우
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 3으로 나누는 경우
         }
-    
         bw.write(String.valueOf(dp[n]));
         bw.flush();
         bw.close();
         br.close();
-  
     }
 }
