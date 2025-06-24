4b5bc536e40561ff4402d59a6a0398fd614bfcf8
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-06-24 09:42:23 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,25 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-
-        Arrays.fill(dp,-1);
-        dp[0] = 0;
-
-        for(int i=1; i<=n; i++){
-            if(i>=3 && dp[i-3]!=-1){
-                dp[i] = dp[i-3]+1;
-            }
-            if(i>=5 && dp[i-5]!=-1){
-                if(dp[i]==-1 || dp[i]>dp[i-5]+1){
-                    dp[i] = dp[i-5]+1;
+        int[] dp = new int[n + 1];
+        if(n >= 3) dp[3] = 1;
+        if(n >= 5) dp[5] = 1;
+        for(int i = 6; i <= n; i++) {
+            if(dp[i-3] != 0) dp[i] = dp[i-3] + 1;
+            if(dp[i-5] != 0) {
+                if(dp[i] == 0 || dp[i-5] + 1 < dp[i]) {
+                    dp[i] = dp[i-5] + 1;
                 }
             }
         }
-        bw.write(String.valueOf(dp[n]));
+        if(dp[n] == 0) bw.write("-1");
+        else bw.write(String.valueOf(dp[n]));
         bw.flush();
         bw.close();
         br.close();
-  
     }
 }
