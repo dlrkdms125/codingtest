3ad0bf59eb8e1b8ba11e1bf0f646a1e7e19bf9a3
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-01 09:53:13 +0900

    Title: 정수의 최솟값, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 250701/정수의 최솟값/minimum-value-of-an-integer.java
+++ 250701/정수의 최솟값/minimum-value-of-an-integer.java
@@ -19,11 +19,11 @@ public class Main {
     }
     private static int add(int a, int b, int c){
         int answer = 0;
-        if(a>b && b>c) answer = c;
-        else if(a>c && c>b) answer = b;
-        else if(b>a && a>c) answer = c;
-        else if(b>c && c>a) answer = a;
-        else if(c>a && a>b) answer = b;
+        if(a>=b && b>=c) answer = c;
+        else if(a>=c && c>=b) answer = b;
+        else if(b>=a && a>=c) answer = c;
+        else if(b>=c && c>=a) answer = a;
+        else if(c>=a && a>=b) answer = b;
         else answer = a;
 
         return answer;
