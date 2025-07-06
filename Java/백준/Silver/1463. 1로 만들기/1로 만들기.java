4352fa46e0ecbc31d8d425b0f0f94971236d103c
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-06 17:28:46 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18144 KB -BaekjoonHub

--- Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -5,19 +5,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        dp[1] = 0; // 1이면 연산 횟수는 0이다
+        int[] dp = new int[n+2];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1]+1;
-            if(i%2==0){
-                dp[i] = Math.min(dp[i], dp[i/2]+1);
-            }
-            if(i%3==0){
-                dp[i] = Math.min(dp[i], dp[i/3]+1);
-            }
+        dp[1] = 0;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + 1;
+            if(i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if(i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
         bw.write(String.valueOf(dp[n]));
-        bw.close();
+        bw.flush();
+
     }
 }
