5345d807c19066d26e1798d3aa030bfdda43f260
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-07 09:16:40 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 24140 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,30 +4,27 @@ import java.io.*;
 
 
 class Main {
-    static int arr[];
-    static int dp[];
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new int[n];
-        String line[] = br.readLine().split(" ");
-
-        dp[0] = Integer.parseInt(line[0]);
-        int max_val = dp[0];
-
-        for(int i=1; i<n; i++){
-            arr[i] = Integer.parseInt(line[i]);
-            dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
-            max_val = Math.max(max_val, dp[i]);
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-    
-        bw.write(String.valueOf(max_val));
+        int[] dp = new int[n+2];
+        dp[0] = 0;
+        dp[1] = arr[0];
+        for(int i=1; i<=n; i++){
+            dp[i] = Math.max(dp[i-1]+arr[i-1], arr[i-1]);
+        }
+        int max = Integer.MIN_VALUE;
+        for(int i=1; i<=n; i++){
+            if(dp[i]>max) max = dp[i];
+        }
+        bw.write(String.valueOf(max));
         bw.flush();
-        bw.close();
-        br.close();
         
     }
 }
