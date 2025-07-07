62124907ff033171939a105fffa9cf0351a377b3
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-07 22:41:47 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- Java/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ Java/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,25 +1,27 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            int[] dp = new int[11];
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int j = 4; j <= n; j++) {
-                dp[j] = dp[j-3]+dp[j-2]+dp[j-1];
-            }
-            bw.write(dp[n]+"\n");
+        int[] arr = new int[t];
+        for(int i=0; i<t; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
+        int[] dp = new int[13];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<=11; i++) {
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        for(int i=0; i<t; i++){
+            bw.write(String.valueOf(dp[arr[i]]));
+            bw.newLine();
+        }   
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
