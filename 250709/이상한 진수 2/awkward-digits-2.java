f85f1700dde6ba6a18f277ed2cac7d86fe910591
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-09 08:03:51 +0900

    Title: 이상한 진수 2, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

--- 250709/이상한 진수 2/awkward-digits-2.java
+++ 250709/이상한 진수 2/awkward-digits-2.java
@@ -7,13 +7,21 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] input = br.readLine().split("");
         int n = input.length;
+        boolean changed = false; // 바꿨는지 체크
+
         for(int i=0; i<n; i++){
             if(input[i].equals("1")) continue;
             else {
                 input[i] = "1";
+                changed = true;
                 break;
             }
         }
+        // 만약 한 번도 바꾸지 않았다면, 마지막 1을 0으로 바꾼다
+        if(!changed) {
+            input[n-1] = "0";
+        }
+
         StringBuilder sb = new StringBuilder();
         for(String s: input){
             sb.append(s);
@@ -21,6 +29,5 @@ public class Main {
         int result = Integer.parseInt(sb.toString(),2);
         bw.write(String.valueOf(result));
         bw.flush();
-    
     }
 }
