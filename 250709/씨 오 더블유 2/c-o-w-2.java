91a87142c3b81ec9fd0077538f29527d00f71269
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-09 15:14:46 +0900

    Title: 씨 오 더블유 2, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

--- 250709/씨 오 더블유 2/c-o-w-2.java
+++ 250709/씨 오 더블유 2/c-o-w-2.java
@@ -15,16 +15,16 @@ public class Main {
             cCount[i] = cCount[i-1] + (input.charAt(i)=='C' ? 1: 0);
         }
         wCount[n-1] = input.charAt(n-1)=='W'? 1:0;
-        for(int i=n-2; n>=0; i--){
+        for(int i=n-2; i>=0; i--){ // <-- 여기 수정!
             wCount[i] = wCount[i+1] + (input.charAt(i)=='W' ? 1:0);
         }
         int cnt = 0;
         for(int i=1; i<n-1; i++){
-            if(input.charAt(i)=='O') cnt += cCount[i-1]+wCount[i+1];
+            if(input.charAt(i)=='O') cnt += cCount[i-1]*wCount[i+1];
         }
-
 
         bw.write(String.valueOf(cnt));
         bw.flush();
     }
 }
+
