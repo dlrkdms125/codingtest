8be8569499eed64a7e55de261357cc99fcfe707d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-09 16:58:41 +0900

    Title: 구간 중 최대 합, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

--- 250709/구간 중 최대 합/max-sum-of-subarray.java
+++ 250709/구간 중 최대 합/max-sum-of-subarray.java
@@ -14,7 +14,7 @@ public class Main {
             arr[i] = Integer.parseInt(st1.nextToken());
         }
         int max = 0;
-        for(int i=0; i<n-k; i++){
+        for(int i=0; i<=n-k; i++){
             int sum = 0;
             for(int j=i; j<i+k; j++){
                 sum += arr[j];
