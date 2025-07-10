ba40601bf5c84bcb61ff918cdf658d221c2ef058
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-10 17:15:01 +0900

    Title: 십진수로 변환하기, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 250710/십진수로 변환하기/convert-to-decimal.java
+++ 250710/십진수로 변환하기/convert-to-decimal.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        String input = String.valueOf(n);
-        int[] arr = new int[n];
+    
+        String input = br.readLine();
+        int[] arr = new int[input.length()];
         for(int i=0; i<input.length(); i++){
             arr[i] = Integer.parseInt(String.valueOf(input.charAt(i)));
         }
