43e26a3c515be9947753de288ee82ce9f99cdacb
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-11 08:32:36 +0900

    Title: 블럭쌓는 명령2, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250711/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] block = new int[n+1];
+        for(int i=0; i<n; i++){
+            block[i] = 0;
+        }
+        int max = 0;
+        for(int i=0; i<k; i++){
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st1.nextToken());
+            int b = Integer.parseInt(st1.nextToken());
+            for(int j=a; j<=b; j++){
+                block[i]++;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(block[i]>max) max = block[i];
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+}
