a03dbb341640dfadda3e20cc6867bb4f72f22369
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-11 09:35:07 +0900

    Title: 왔다 갔던 구역 2, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

--- 250711/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 250711/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -6,29 +6,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] line = new int[1001]; // 구간 체크용 (좌표 0~1000)
+        int[] line = new int[1001];
         int cnt = 0;
         int now = 500;
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int number  = Integer.parseInt(st.nextToken());
+            int number = Integer.parseInt(st.nextToken());
             String dir = st.nextToken();
             if(dir.equals("R")) {
-                for(int j=0; j<number; j++) {
-                    int left = now + j;
-                    int right = now + j + 1;
-                    line[Math.min(left, right)]++;
+                for(int j=0; j<number; j++){
+                    line[now+j]++;
                 }
-                now = now + number;
+                now = now+number;
             } else {
-                for(int j=0; j<number; j++) {
-                    int left = now - j;
-                    int right = now - j - 1;
-                    line[Math.min(left, right)]++;
+                for(int j=0; j<number; j++){
+
+                    line[now-j-1]++;
                 }
                 now = now - number;
             }
         }
+
         for(int i=0; i<line.length; i++){
             if(line[i]>=2) cnt++;
         }
