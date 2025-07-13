a326da682c03609568fde10f707375b342678b57
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-13 14:11:24 +0900

    Title: 연속되는 수 3, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250713/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int max = 1;
+        int cnt = 1;
+        for(int i=1; i<n; i++){
+            if((arr[i]>0 && arr[i-1]>0) || (arr[i]<0 && arr[i-1]<0)) {
+                cnt++;
+                if(cnt>max) max = cnt;
+            } else {
+                cnt = 1;
+            }
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+}
