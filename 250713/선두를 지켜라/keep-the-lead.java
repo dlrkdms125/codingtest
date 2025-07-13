de32272bf24acd3d6277a4921694dc9387b842a6
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-13 16:39:35 +0900

    Title: 선두를 지켜라, Time: 115ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250713/선두를 지켜라/keep-the-lead.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] am = new int[100000];
+        int[] bm = new int[100000];
+        int total = 0;
+        for(int i=1; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            for(int j=0; j<t; j++){
+                am[i] = am[i-1]+v;
+            }
+            total += v;
+        }
+        for(int i=1; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            for(int j=0; j<t; j++){
+                bm[i] = bm[i-1]+v;
+            }
+        }
+        int cnt = 1;
+        for(int i=0; i<total; i++){
+            if(am[i]<bm[i]) cnt++;
+        }
+
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+
+    }
+}
