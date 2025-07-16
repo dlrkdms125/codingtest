5355e04d9d94cd7bd6997ee98df98bf3202d1c0d
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-16 11:25:12 +0900

    Title: 1이 3개 이상 있는 위치, Time: 104ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250716/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    // 북동남서
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static int n;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        int[][] map = new int[n][n];
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int x = 0;
+        int y = 0;
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(inRange(nx, ny) && map[nx][ny]==1) cnt++;
+            }
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+
+
+    }
+    private static boolean inRange(int x, int y){
+        return (x>=0 && x<n && y>=0 && y<n);
+    }
+
+}
