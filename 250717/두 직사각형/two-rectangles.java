0732b9d1924892a2602866aa288c1b7d6e1d5c25
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-17 15:49:32 +0900

    Title: 두 직사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250717/두 직사각형/two-rectangles.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+        st1 = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st1.nextToken());
+        int b1 = Integer.parse
+
+
+        bw.write();
+        bw.flush();
+
+    }
+}
