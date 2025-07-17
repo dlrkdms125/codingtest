3d0907ce31a2486a636422a56ad19585775d8fc5
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-17 15:41:55 +0900

    Title: 두 선분, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250717/두 선분/two-lines.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int x3 = Integer.parseInt(st.nextToken());
+        int x4 = Integer.parseInt(st.nextToken());
+        if(x2<x3 || x1>x4) bw.write("nonintersecting");
+        else bw.write("intersecting");
+        bw.flush();
+
+    }
+}
