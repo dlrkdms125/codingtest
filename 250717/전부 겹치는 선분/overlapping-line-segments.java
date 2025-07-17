f82db1d55127099c3580859110a9bcc7b75e3c52
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-17 16:15:07 +0900

    Title: 전부 겹치는 선분, Time: 123ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250717/전부 겹치는 선분/overlapping-line-segments.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] line = new int[102];
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            for(int j=x1-1; j<x2; j++){
+                line[j]++;
+            }
+        }
+        String answer = "";
+        for(int i=0; i<=101; i++){
+            if(line[i]>=3) {
+                answer = "Yes";
+                break;
+            } else answer = "No";
+        }
+        bw.write(answer);
+        bw.flush();
+    }
+}
