fc382978c68744d257850e48120c7b5001df3bc8
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-23 12:29:46 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 250723/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 250723/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -2,18 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int[] arr = new int[100];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int max = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for(int i=0; i<n; i++){
-            int number = Integer.parseInt(st.nextToken());
-            if(max<number) max = number;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        bw.write(String.valueOf(max));
+        
+        bw.write(String.valueOf(max(n-1)));
         bw.flush();
         bw.close();
+    }
+    private static int max(int n){
+        if(n==0) return arr[0];
+        return Math.max(max(n-1), arr[n]);
     }
 }
