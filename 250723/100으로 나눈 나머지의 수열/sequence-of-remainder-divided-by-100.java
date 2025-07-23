b7003f6d17d0d4cbc9c11c9a607a682d52a22001
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-23 13:11:46 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

--- 250723/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
+++ 250723/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -2,21 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] arr = new int[100];
+    static int[] arr = new int[101];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
         int n = Integer.parseInt(br.readLine());
+        Arrays.fill(arr,-1);
         int result = print(n)%100;
         bw.write(String.valueOf(result));
         bw.flush();
 
     }
     private static int print(int n) throws IOException {
-        int answer = 0;
-        if(n==1) answer = 2;
-        if(n==2) answer =  4;
-        if(n>=3) answer = print(n-1)*print(n-2);
-        return answer;
+        if(arr[n]!=-1) return arr[n];
+        if(n==1) return arr[n]=2;
+        if(n==2) return arr[n]=4;
+
+        arr[n] = (print(n-1)*print(n-2))%100;
+        return arr[n];
     }
 }
