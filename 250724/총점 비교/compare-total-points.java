318b838cfeea3610283aeacdd1a15f16e75c2d22
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-24 14:50:38 +0900

    Title: 총점 비교, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250724/총점 비교/compare-total-points.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+class Student implements Comparable<Student> {
+    String name;
+    int kor;
+    int eng;
+    int math;
+    public Student(String name, int kor, int eng, int math){
+        this.name=name;
+        this.kor=kor;
+        this.eng=eng;
+        this.math=math;
+    }
+    @Override
+    public int compareTo(Student student) {  
+        return (this.kor + this.eng + this.math) - (student.kor + student.eng + student.math);
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        int n = Integer.parseInt(br.readLine());
+        Student[] students = new Student[n];
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int kor = Integer.parseInt(st.nextToken());
+            int eng = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+            students[i] = new Student(name, kor, eng, math);
+        }
+        Arrays.sort(students);
+        for(int i=0; i<n; i++){
+            bw.write(students[i].name+" "+students[i].kor+" "+students[i].eng+" "+students[i].math);
+            bw.newLine();
+        }
+        bw.flush();
+
+    }
+}
