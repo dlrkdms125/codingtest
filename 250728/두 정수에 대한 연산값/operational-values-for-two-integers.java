13176c7504b0716158f14b442f26fe473d81e6df
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-28 10:36:53 +0900

    Title: 두 정수에 대한 연산값, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250728/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int[] result = cal(a,b);
+        bw.write(result[0]+" "+result[1]);
+        bw.flush();
+
+    }
+    private static int[] cal(int a, int b){
+        if(a<b) {
+            b += 25;
+            a *= 2;
+        } else {
+            b *= 2;
+            a += 25;
+        }
+        return new int[]{a,b};
+    }
+}
