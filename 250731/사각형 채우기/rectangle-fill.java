029f8beccdd683143c2b7839eae8f4796aadb4a1
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-31 16:38:58 +0900

    Title: 사각형 채우기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

--- 250731/사각형 채우기/rectangle-fill.java
+++ 250731/사각형 채우기/rectangle-fill.java
@@ -10,9 +10,9 @@ public class Main {
         if(n>=2) dp[2] = 2;
         if(n>=3) dp[3] = 3;
         for(int i=4; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
         }
-        bw.write(String.valueOf(dp[n]%10007));
+        bw.write(String.valueOf(dp[n]));
         bw.flush();
 
     }
