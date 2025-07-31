f2ef0601681728777af7067d36a43562fb157184
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-31 16:28:56 +0900

    Title: 계단 오르기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250731/계단 오르기/climbing-stairs.java
+++ 250731/계단 오르기/climbing-stairs.java
@@ -7,13 +7,15 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n+1];
+        dp[0] = 1;
+        if(n>=1) dp[1] = 0;
         if(n>=2) dp[2] = 1;
         if(n>=3) dp[3] = 1;
-        if(n>=4) dp[4] = 1;
-        if(n>=5) dp[5] = 2;
-        for(int i=6; i<=n; i++){
+        
+        for(int i=4; i<=n; i++){
             dp[i] = dp[i-2]+dp[i-3];
         }
+
         bw.write(String.valueOf(dp[n]%10007));
         bw.flush();
     }
