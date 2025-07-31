bd23bff60b53090c00dbee1aa2a5d977bed788ba
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-31 16:53:40 +0900

    Title: 사각형 채우기 3, Time: 82ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250731/사각형 채우기 3/rectangle-fill-3.java
+++ 250731/사각형 채우기 3/rectangle-fill-3.java
@@ -12,7 +12,7 @@ public class Main {
         if(n>=2) dp[2] = 7;
         if(n>=2)dp[3] = 22;
         for(int i=4; i<=n; i++){
-            dp[i] = (dp[i-1]*4-dp[i-1]+1)%1_000_000_007;
+            dp[i] = (dp[i-1]*2+dp[i-2])%1_000_000_007;
         }
 
         bw.write(String.valueOf(dp[n]));
