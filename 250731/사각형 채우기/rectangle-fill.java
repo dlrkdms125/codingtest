62812ce1e7168087aa8b4504df7833c9d3d71a95
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-07-31 16:38:18 +0900

    Title: 사각형 채우기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250731/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        if(n>=2) dp[2] = 2;
+        if(n>=3) dp[3] = 3;
+        for(int i=4; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        bw.write(String.valueOf(dp[n]%10007));
+        bw.flush();
+
+    }
+}
