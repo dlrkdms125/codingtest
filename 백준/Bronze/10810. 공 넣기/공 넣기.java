1e1d17574031ba3ad8143109db5638a2931e55c9
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-08-07 16:41:25 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,31 +1,28 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main {
+
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] num = new int[n];
-
-        for (int i = 0; i < m; i++) {
+        int[] bucket = new int[n+1];
+        for(int t=0; t<m; t++){
             st = new StringTokenizer(br.readLine());
-            int I = Integer.parseInt(st.nextToken());
-            int J = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-
-            for (int j = I-1; j < J; j++) {
-                num[j] = K;
+            int i = Integer.parseInt(st.nextToken())-1;
+            int j = Integer.parseInt(st.nextToken())-1;
+            int k = Integer.parseInt(st.nextToken());
+            for(int z=i; z<=j; z++){
+                bucket[z] = k;
             }
         }
-        for (int i = 0; i < num.length; i++) {
-            bw.write(num[i]+" ");
+        for(int i=0; i<n; i++){
+            bw.write(bucket[i]+" ");
         }
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
