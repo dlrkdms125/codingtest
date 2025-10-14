d9c0313d51dd53545a3f305de5a5c60764c335df
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-10-14 16:02:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str = Integer.toString(n);
-        for(int i=0; i<str.length(); i++){
-            answer += Integer.parseInt(String.valueOf(str.charAt(i)));
+        String[] var = Integer.toString(n).split("");
+        for(int i=0; i<var.length; i++){
+            int number = Integer.parseInt(var[i]);
+            answer += number;
         }
         return answer;
     }
