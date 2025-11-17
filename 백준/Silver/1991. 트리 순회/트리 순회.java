e63c45fc0d6734f8d3c89a4983f8db44246169f6
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-11-18 08:42:54 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -3,41 +3,39 @@ import java.lang.*;
 import java.io.*;
 
 class Node {
-    char value;
+    char val;
     Node left;
     Node right;
-
-    public Node(char value){
-        this.value=value;
+    public Node(char val){
+        this.val=val;
         this.left = null;
         this.right = null;
     }
 }
 
-
 class Main {
     static Node[] tree;
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    // 전위 순회
+    // 전위순회
     public static void preorder(Node node) throws IOException {
         if(node==null) return;
-        bw.write(node.value);
+        bw.write(node.val);
         preorder(node.left);
         preorder(node.right);
     }
-    // 중위 순회
+    // 중위순회
     public static void inorder(Node node) throws IOException {
         if(node==null) return;
         inorder(node.left);
-        bw.write(node.value);
+        bw.write(node.val);
         inorder(node.right);
     }
-    // 후위 순회
+    // 후위순회
     public static void postorder(Node node) throws IOException {
         if(node==null) return;
         postorder(node.left);
         postorder(node.right);
-        bw.write(node.value);
+        bw.write(node.val);
     }
     
     public static void main(String[] args) throws IOException {
@@ -49,15 +47,14 @@ class Main {
             char parentValue = st.nextToken().charAt(0);
             char leftValue = st.nextToken().charAt(0);
             char rightValue = st.nextToken().charAt(0);
-
-            if(tree[parentValue-'A']==null) { // 부모 노드가 아직 생성되지 않은 경
-                tree[parentValue-'A'] = new Node(parentValue); // 부모 노드 생성
+            if(tree[parentValue-'A']==null) {
+                tree[parentValue-'A'] = new Node(parentValue);
             }
-            if(leftValue !='.') {
+            if(leftValue != '.') {
                 tree[leftValue-'A'] = new Node(leftValue);
                 tree[parentValue-'A'].left = tree[leftValue-'A'];
-            }
-            if(rightValue !='.') {
+            }    
+            if(rightValue != '.'){
                 tree[rightValue-'A'] = new Node(rightValue);
                 tree[parentValue-'A'].right = tree[rightValue-'A'];
             }
@@ -68,8 +65,8 @@ class Main {
         bw.newLine();
         postorder(tree[0]);
         bw.newLine();
-
         bw.flush();
+        bw.close();
         
     }
 }
