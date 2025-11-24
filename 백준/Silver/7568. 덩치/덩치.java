4bd6e2e585bde496eb7205104c36cf55d3874d71
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-11-24 10:53:28 +0900

    [Silver V] Title: 덩치, Time: 96 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -8,21 +8,24 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         int[][] people = new int[n][2];
         for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            people[i][0] = Integer.parseInt(st.nextToken());
-            people[i][1] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            people[i][0] = weight;
+            people[i][1] = height;
         }
         for(int i=0; i<n; i++){
             int cnt = 0;
             for(int j=0; j<n; j++){
-                if(i==j) continue;
                 if(people[i][0]<people[j][0] && people[i][1]<people[j][1]) cnt++;
             }
-            bw.write((cnt+1)+" ");
+            bw.write(String.valueOf(1+cnt)+" ");
         }
         bw.flush();
-        
+        bw.close();
+        br.close();
     }
 }
