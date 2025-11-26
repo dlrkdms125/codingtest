e9c5f80a64bc372c8125a04de742a58b4cfa8118
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-11-26 10:16:28 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,38 +1,39 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main {
+
+class Main {
     static int n,m;
-    static int[][] map;
+    static int[][] arr;
     static boolean[] vi;
-    static int count = 0;
-
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
-        map = new int[n+1][n+1];
+        StringTokenizer st;
+        arr = new int[n+1][n+1];
         vi = new boolean[n+1];
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map[a][b] = map[b][a] = 1;
-
+            arr[a][b] = arr[b][a] = 1;
         }
         dfs(1);
-        bw.write(String.valueOf(count-1));
+        bw.write(String.valueOf(cnt));
         bw.flush();
         bw.close();
         br.close();
-
+        
     }
-    private static void dfs(int v) {
-        vi[v] = true;
-        count++;
-        for (int i = 1; i <= n; i++) {
-            if(map[v][i] == 1 && !vi[i]){
+    private static void dfs(int start){
+        vi[start] = true;
+        for(int i=0; i<=n; i++){
+            if(arr[start][i]==1 && !vi[i]) {
+                cnt++;
                 dfs(i);
             }
         }
