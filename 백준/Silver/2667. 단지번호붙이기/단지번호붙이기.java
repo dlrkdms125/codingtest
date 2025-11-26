6e0993711c6d6fdad5cb30f36d7eafdcfb5c1023
dlrkdms125 <165174659+dlrkdms125@users.noreply.github.com>
2025-11-26 10:34:51 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,47 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
 
-    static int[][] danji;
+class Main {
+    static int n;
+    static int[][] arr;
     static boolean[][] vi;
-    static int[] dx = {0, 0, -1, 1}; 
-    static int[] dy = {-1, 1, 0, 0}; 
-    static List<Integer> result; 
-    static int cnt, N; 
-
+    static int cnt;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,-1,1};
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder(); 
-        result = new ArrayList<>(); 
-        N = Integer.parseInt(br.readLine());
-        danji = new int[N][N]; 
-        vi = new boolean[N][N]; 
-
-     
-        for (int i = 0; i < N; i++) {
-            sb = new StringBuilder(br.readLine());
-            for (int j = 0; j < N; j++) {
-                danji[i][j] = sb.charAt(j) - '0'; 
+        
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        vi = new boolean[n][n];
+        
+        for(int i=0; i<n; i++){
+            String line = br.readLine();
+            for(int j=0; j<n; j++){
+                arr[i][j] = line.charAt(j)-'0';
             }
         }
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
-                if (danji[x][y] == 1 && !vi[x][y]) { 
+        // 전체 지도 탐색
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j]==1 && !vi[i][j]) {
                     cnt = 0;
-                    dfs(x, y); 
-                    result.add(cnt); 
+                    dfs(i,j);
+                    list.add(cnt);
                 }
             }
         }
-
-        Collections.sort(result); 
-
-        bw.write(String.valueOf(result.size()));
+        Collections.sort(list);
+        bw.write(String.valueOf(list.size()));
         bw.newLine();
-        for (int num : result) {
+        for(int num: list){
             bw.write(String.valueOf(num));
             bw.newLine();
         }
@@ -49,18 +48,14 @@ public class Main {
         bw.close();
         br.close();
     }
-
-    public static void dfs(int x, int y) {
-        vi[x][y] = true; // 현재 위치 방문 처리
-        cnt++; // 현재 단지에 속한 집 개수 증가
-
-        for (int i = 0; i < 4; i++) { 
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !vi[nx][ny] && danji[nx][ny] == 1) {
-                dfs(nx, ny); // 연결된 집 발견 시 재귀적으로 탐색 진행
-            }
+    private static void dfs(int x, int y){
+        vi[x][y] = true;
+        cnt++;
+        for(int d=0; d<4; d++){
+            int nx = x+dx[d];
+            int ny = y+dy[d];
+            if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+            if(!vi[nx][ny] && arr[nx][ny]==1) dfs(nx, ny);
         }
     }
 }
